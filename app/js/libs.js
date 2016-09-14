bbaf9cd1056300216b4046864c6f48b1182718cd
Lulca <Anatolkruv@gmail.com>
2016-09-14 23:47:09 +0300

    initial commit

---
+++ app/js/libs.js
@@ -0,0 +1,3 @@
+//@prepros-append ../libs/jquery-1.12.3/jquery-1.12.3.js
+//@prepros-append ../libs/bootstrap-3.3.6-dist/js/bootstrap.js
+//@prepros-append ../libs/Magnific-Popup-master/dist/jquery.magnific-popup.js
