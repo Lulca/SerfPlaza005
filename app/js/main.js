3fdd5db3d50cb712452912ba07417a8e46597d20
Lulca <Anatolkruv@gmail.com>
2016-09-17 00:05:10 +0300

    make responcive top navigation

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
+var siteNavigation = (function() {
+
+	return {
+
+		init: function() {
+			var menu = $('.menu');
+			var button = $('.icon-menu2');
+
+			button.on('click', function() {
+				menu.fadeToggle();
+			});
+
+			$(window).resize(function(){
+				if ($(window).width() >= 1200) {
+					menu.fadeIn(.1);
+				}	
+			});
+			
+		}
+	}
+})();
+
+
 $(document).ready(function() {
-	
+	siteNavigation.init();
 });
